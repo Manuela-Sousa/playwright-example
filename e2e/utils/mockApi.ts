c2c226f6b73039965c1b7d2499d6e6d43980d353
Manuela Sousa <manuelacatsousa@gmail.com>
2024-11-10 09:50:54 +0000

    test: add type for mockApi request

--- e2e/utils/mockApi.ts
+++ e2e/utils/mockApi.ts
@@ -1,10 +1,16 @@
-import { expect } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
+
+type MockResponseData = Record<string, any>;
+type Endpoint = string | RegExp;
+type StatusCode = number;
+type ContentType = string;
+
 export const mockApiRequest = async (
-  page,
-  endpoint,
-  responseData,
-  statusCode,
-  contentType
+  page: Page,
+  endpoint: Endpoint,
+  responseData: MockResponseData,
+  statusCode: StatusCode,
+  contentType: ContentType 
 ) => {
   await page.route(endpoint, async (route) => {
     await route.fulfill({
