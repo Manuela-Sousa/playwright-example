b5b99ab3c8ebc91f89125691c329c26fd486f064
Manuela Sousa <manuelacatsousa@gmail.com>
2025-05-21 13:59:27 +0100

    feat: fix file name

--- e2e/pages/homepage.ts
+++ e2e/pages/homepage.ts
@@ -2,7 +2,7 @@ import { Page } from "playwright-core";
 import { locators } from "../locators/locators";
 import BasePage from "./BasePage";
 
-export default class HomePage extends BasePage {
+export default class homePage extends BasePage {
   constructor(page: Page) {
     super(page);
   }
