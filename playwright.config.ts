074c57a9e9a2d94937213f4060cfe1492d00bbd3
manuelacatsousa@gmail.com <manuelacatsousa@gmail.com>
2025-05-28 20:59:49 +0100

    feat: putting headless false

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ try {
 export default defineConfig({
   testDir: "./e2e",
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
